8d75f95325dc998419e411ac2a247ad5d872b132
azambembem <azamjan980713@gmail.com>
2025-01-21 02:25:21 +0900

    categories & images

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Flayer from "./components/flayer";
 import Navbar from "./components/navbar";
+import { Toaster } from "@/components/ui/sonner";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Toaster />
+      <Flayer />
     </div>
   );
 }
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,12 @@
 import type { FC } from "react";
 import { Input } from "../ui/input";
+import { Heart, Search, ShoppingCart } from "lucide-react";
+import { toast } from "sonner";
 
 const Navbar: FC = () => {
   return (
-    <div className="flex justify-between items-center w-[90%] h-[50px] mx-auto mt-4">
-      <div className="text-[20px] font-bold text-black flex items-center">
+    <div className="flex justify-between items-center w-[90%] h-[50px] mx-auto mt-4 ">
+      <div className="text-[20px] font-bold text-black flex items-center ml-4">
         Exclusive
       </div>
 
@@ -17,20 +19,23 @@ const Navbar: FC = () => {
 
       <div className="flex items-center gap-6">
         <div className="relative mt-4">
-          <Input
+          <Input // shadcn/ui 사용하기기
             type="text"
             placeholder="What are you looking for?"
             className="w-[250px] h-[30px] pl-4 pr-5 border border-gray-300 rounded-sm text-sm bg-[#f5f5f5] focus:outline-none"
           />
-          <img
-            src={"/icons/search.svg"}
-            alt="search"
-            className="absolute inset-y-1.5 right-3 flex items-center w-5 h-5"
-          />
+
+          <Search className="absolute inset-y-1.5 right-3 flex items-center w-5 h-5" />
         </div>
         <div className="flex gap-4 mt-4">
-          <img src={"/icons/heart.svg"} alt="heart" className="w-5 h-5" />
-          <img src={"/icons/cart.svg"} alt="cart" className="w-5 h-5" />
+          <Heart
+            onClick={() => toast("Like")}
+            className="w-5 h-5 cursor-pointer"
+          />
+          <ShoppingCart
+            onClick={() => toast("Add to Cart")}
+            className="w-5 h-5 cursor-pointer"
+          />
         </div>
       </div>
     </div>
